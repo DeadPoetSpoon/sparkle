9d47eca24d969c6aa2419fc2ff73884fb9b2ac0d
DeadPoetSpoon <deadpoetspoon@qq.com>
2026-07-08 08:53:46 +0800

    Add War and Peace character dataset

---
+++ data/202607080842/default.js
@@ -0,0 +1,598 @@
+export default [
+  // 主要人物
+  {
+    data: {
+      id: "a1",
+      name: "皮埃尔·别祖霍夫",
+      nickname: ["皮埃尔", "别祖霍夫伯爵"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "a2",
+      name: "安德烈·保尔康斯基",
+      nickname: ["安德烈公爵", "安德烈"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "a3",
+      name: "娜塔莎·罗斯托娃",
+      nickname: ["娜塔莎"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "a4",
+      name: "尼古拉·罗斯托夫",
+      nickname: ["尼古拉"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "a5",
+      name: "玛丽亚·保尔康斯卡娅",
+      nickname: ["玛丽亚公爵小姐", "玛丽亚"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "a6",
+      name: "海伦·库拉金娜",
+      nickname: ["海伦", "别祖霍娃伯爵夫人"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "a7",
+      name: "阿纳托尔·库拉金",
+      nickname: ["阿纳托尔"],
+      gender: "男",
+    },
+  },
+
+  // 罗斯托夫家族
+  {
+    data: {
+      id: "b1",
+      name: "伊利亚·罗斯托夫伯爵",
+      nickname: ["老伯爵"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "b2",
+      name: "娜塔莉娅·罗斯托娃伯爵夫人",
+      nickname: ["伯爵夫人"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "b3",
+      name: "薇拉·罗斯托娃",
+      nickname: ["薇拉"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "b4",
+      name: "彼佳·罗斯托夫",
+      nickname: ["彼佳"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "b5",
+      name: "索尼娅",
+      nickname: ["索涅奇卡"],
+      gender: "女",
+    },
+  },
+
+  // 保尔康斯基家族
+  {
+    data: {
+      id: "c1",
+      name: "尼古拉·保尔康斯基老公爵",
+      nickname: ["老公爵"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "c2",
+      name: "丽莎·保尔康斯卡娅",
+      nickname: ["矮小的公爵夫人", "丽莎"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "c3",
+      name: "小尼古拉",
+      nickname: ["尼古卢什卡"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "c4",
+      name: "布里安小姐",
+      nickname: ["女伴"],
+      gender: "女",
+    },
+  },
+
+  // 库拉金家族
+  {
+    data: {
+      id: "d1",
+      name: "瓦西里·库拉金公爵",
+      nickname: ["瓦西里公爵"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "d2",
+      name: "伊波利特·库拉金",
+      nickname: ["伊波利特"],
+      gender: "男",
+    },
+  },
+
+  // 德鲁别茨科伊家族
+  {
+    data: {
+      id: "e1",
+      name: "鲍里斯·德鲁别茨科伊",
+      nickname: ["鲍里斯"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "e2",
+      name: "安娜·米哈伊洛芙娜",
+      nickname: ["德鲁别茨卡娅公爵夫人"],
+      gender: "女",
+    },
+  },
+
+  // 别祖霍夫家族
+  {
+    data: {
+      id: "f1",
+      name: "基里尔·别祖霍夫伯爵",
+      nickname: ["老别祖霍夫"],
+      gender: "男",
+    },
+  },
+
+  // 其他重要人物
+  {
+    data: {
+      id: "g1",
+      name: "多洛霍夫",
+      nickname: ["费佳"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "g2",
+      name: "瓦西里·杰尼索夫",
+      nickname: ["杰尼索夫"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "g3",
+      name: "安娜·帕夫洛芙娜·舍列尔",
+      nickname: ["宫廷女官", "安娜·帕夫洛芙娜"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "g4",
+      name: "库图佐夫",
+      nickname: ["总司令", "元帅"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "g5",
+      name: "拿破仑",
+      nickname: ["拿破仑一世"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "g6",
+      name: "亚历山大一世",
+      nickname: ["沙皇"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "g7",
+      name: "巴格拉季翁",
+      nickname: ["将军"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "g8",
+      name: "普拉东·卡拉塔耶夫",
+      nickname: ["普拉东"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "g9",
+      name: "朱丽·卡拉金娜",
+      nickname: ["朱丽"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "g10",
+      name: "别格",
+      nickname: ["军官"],
+      gender: "男",
+    },
+  },
+  {
+    data: {
+      id: "g11",
+      name: "玛丽亚·德米特里耶芙娜·阿赫罗西莫娃",
+      nickname: ["阿赫罗西莫娃"],
+      gender: "女",
+    },
+  },
+  {
+    data: {
+      id: "g12",
+      name: "阿尔帕特奇",
+      nickname: ["管家"],
+      gender: "男",
+    },
+  },
+
+  // 关系 - 罗斯托夫家族
+  {
+    data: {
+      source: "b1",
+      target: "b2",
+      name: "丈夫",
+    },
+  },
+  {
+    data: {
+      source: "a3",
+      target: "b1",
+      name: "女儿",
+    },
+  },
+  {
+    data: {
+      source: "a4",
+      target: "b1",
+      name: "儿子",
+    },
+  },
+  {
+    data: {
+      source: "b3",
+      target: "b1",
+      name: "女儿",
+    },
+  },
+  {
+    data: {
+      source: "b4",
+      target: "b1",
+      name: "儿子",
+    },
+  },
+  {
+    data: {
+      source: "b5",
+      target: "b1",
+      name: "外甥女",
+    },
+  },
+
+  // 关系 - 保尔康斯基家族
+  {
+    data: {
+      source: "a2",
+      target: "c1",
+      name: "儿子",
+    },
+  },
+  {
+    data: {
+      source: "a5",
+      target: "c1",
+      name: "女儿",
+    },
+  },
+  {
+    data: {
+      source: "a2",
+      target: "c2",
+      name: "丈夫",
+    },
+  },
+  {
+    data: {
+      source: "c3",
+      target: "a2",
+      name: "儿子",
+    },
+  },
+  {
+    data: {
+      source: "c4",
+      target: "a5",
+      name: "女伴",
+    },
+  },
+
+  // 关系 - 库拉金家族
+  {
+    data: {
+      source: "a6",
+      target: "d1",
+      name: "女儿",
+    },
+  },
+  {
+    data: {
+      source: "a7",
+      target: "d1",
+      name: "儿子",
+    },
+  },
+  {
+    data: {
+      source: "d2",
+      target: "d1",
+      name: "儿子",
+    },
+  },
+
+  // 关系 - 德鲁别茨科伊家族
+  {
+    data: {
+      source: "e1",
+      target: "e2",
+      name: "儿子",
+    },
+  },
+
+  // 关系 - 别祖霍夫家族
+  {
+    data: {
+      source: "a1",
+      target: "f1",
+      name: "私生子",
+    },
+  },
+
+  // 关系 - 婚姻与爱情
+  {
+    data: {
+      source: "a1",
+      target: "a6",
+      name: "丈夫",
+    },
+  },
+  {
+    data: {
+      source: "a1",
+      target: "a3",
+      name: "丈夫",
+    },
+  },
+  {
+    data: {
+      source: "a2",
+      target: "a3",
+      name: "未婚夫",
+    },
+  },
+  {
+    data: {
+      source: "a4",
+      target: "a5",
+      name: "丈夫",
+    },
+  },
+  {
+    data: {
+      source: "a4",
+      target: "b5",
+      name: "恋人",
+    },
+  },
+  {
+    data: {
+      source: "a7",
+      target: "a3",
+      name: "诱惑者",
+    },
+  },
+  {
+    data: {
+      source: "b3",
+      target: "g10",
+      name: "妻子",
+    },
+  },
+  {
+    data: {
+      source: "e1",
+      target: "g9",
+      name: "丈夫",
+    },
+  },
+
+  // 关系 - 友情与社交
+  {
+    data: {
+      source: "a1",
+      target: "a2",
+      name: "挚友",
+    },
+  },
+  {
+    data: {
+      source: "g2",
+      target: "a4",
+      name: "战友",
+    },
+  },
+  {
+    data: {
+      source: "g2",
+      target: "a3",
+      name: "求婚者",
+    },
+  },
+  {
+    data: {
+      source: "g1",
+      target: "a7",
+      name: "朋友",
+    },
+  },
+  {
+    data: {
+      source: "g1",
+      target: "a1",
+      name: "决斗对手",
+    },
+  },
+  {
+    data: {
+      source: "g1",
+      target: "a6",
+      name: "情人",
+    },
+  },
+  {
+    data: {
+      source: "g1",
+      target: "b5",
+      name: "求婚者",
+    },
+  },
+  {
+    data: {
+      source: "e1",
+      target: "a3",
+      name: "青梅竹马",
+    },
+  },
+  {
+    data: {
+      source: "g3",
+      target: "d1",
+      name: "密友",
+    },
+  },
+  {
+    data: {
+      source: "e2",
+      target: "b2",
+      name: "友人",
+    },
+  },
+  {
+    data: {
+      source: "g11",
+      target: "a3",
+      name: "教母",
+    },
+  },
+
+  // 关系 - 军旅与政治
+  {
+    data: {
+      source: "a2",
+      target: "g4",
+      name: "副官",
+    },
+  },
+  {
+    data: {
+      source: "a4",
+      target: "g4",
+      name: "部下",
+    },
+  },
+  {
+    data: {
+      source: "g7",
+      target: "g4",
+      name: "部下",
+    },
+  },
+  {
+    data: {
+      source: "g4",
+      target: "g6",
+      name: "统帅",
+    },
+  },
+  {
+    data: {
+      source: "g4",
+      target: "g5",
+      name: "对手",
+    },
+  },
+  {
+    data: {
+      source: "g5",
+      target: "g6",
+      name: "敌人",
+    },
+  },
+  {
+    data: {
+      source: "a1",
+      target: "g8",
+      name: "狱友",
+    },
+  },
+
+  // 关系 - 家仆
+  {
+    data: {
+      source: "g12",
+      target: "c1",
+      name: "管家",
+    },
+  },
+];
---
+++ data/202607080842/index-zh.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    version: "default",
+    name: "草婴-上海三联-2014",
+    description: "不错~不错~",
+  },
+];
